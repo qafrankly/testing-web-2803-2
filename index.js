9b511a6301210d44b65e2d4ed18a1b57f3bfd8bf
bent gunnar <bent@franklyinc.com>
2017-08-15 16:55:07 +0000

    frn test

---
+++ index.js
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react';
+
+class RandomTest extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      //h1 = dep(),
+      h2 = 'sup',
+    } = this.props;
+
+    let {
+      flux, config
+    } = this.context;
+
+    return (
+      <div>
+        <h2>{h2}</h2>
+      </div>
+    );
+  }
+}
+
+export default RandomTest;
+
