0e8b52cf6d0cc93cefbca849067c63712bf6eff3
Axel Brock Cardan <30811286+axelbrock@users.noreply.github.com>
2017-11-03 05:46:15 -0700

    Update dep.js

--- dep.js
+++ dep.js
@@ -1,8 +1,26 @@
+import React, { Component, PropTypes } from 'react';
 
+class Dep extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      //h1 = dep(),
+      h2 = 'the dep component'
+    } = this.props;
 
-function hello(){
-  return 'something'
+    let {
+      flux, config
+    } = this.context;
+
+    return (
+      <div>
+        <h2>{h2}</h2>
+      </div>
+    );
+  }
 }
 
-
-export default hello;
+export default Dep;
