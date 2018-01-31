9a48e23501265cd31215cfafca2d6db517bae710
bent gunnar <bent@franklyinc.com>
2018-01-31 10:58:14 +0000

    mv

---
+++ module1/index.js
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react';
+
+class ModuleTwo extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      h2 = 'Hi TL welcome to Custom Modules! :)'
+    } = this.props;
+
+    let {
+      flux, config
+    } = this.context;
+
+    return (
+      <div>
+        <h2>This is module2</h2>
+	<h2>testing ModuleTwo</h2>
+      </div>
+    );
+  }
+}
+
+export default ModuleTwo;
+
